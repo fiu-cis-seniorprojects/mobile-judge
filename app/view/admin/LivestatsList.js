627c16ce369a5a6dce1dcb57b7c18d3e6a311e01
Andy Norcisa <norcisa.andy@hotmail.com>
2014-06-28 20:49:27 -0400

    fixed the livestats template

--- app/view/admin/LivestatsList.js
+++ app/view/admin/LivestatsList.js
@@ -9,21 +9,8 @@ Ext.define("OnlineJudges.view.admin.LivestatsList", {
     config: {
         title: 'Livestats',
         iconCls: 'favorites',
-        itemTpl: [
-        '<div class="x-container x-field-checkbox x-field x-label-align-left x-field-labeled" style="background:none">',
-            '<div class="x-form-label" style="background:none;padding: 0">',
-            '<div align="left"><tpl if= \'LastName != null\'>{Name} {LastName}<tpl else>{Name}</tpl></div>',
-            '<div align="left"  style="font-size:12px"><tpl if= \'LastName != null\'> Student ID: id</tpl></div>', 
-            //'<div align="left"  style="font-size:12px"></div>',
-        '</div>',
-            //'<div class="x-component-outer">',
-            //'<div align="center"><tpl if= \'RawGrade != null\'> Raw Grade: {RawGrade}<tpl else>Raw Grade: N/A</tpl></div>',
-            //'</div>',
-            '<div class="x-component-outer">',
-            //'<div align="right"><tpl if= \'RawGrade != null\'> Raw Grade: {RawGrade}<tpl else>Raw Grade: n/a</tpl> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  <tpl if= \'ApprovedGrade != null\'>Approved Grade: {ApprovedGrade}<tpl else>Approved Grade: n/a</tpl></div>',
-            '<div align="right"><tpl if= \'RawGrade != null\'> Raw Grade: n/a<tpl else>Raw Grade: n/a</tpl> &nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp&nbsp  <tpl if= \'ApprovedGrade != null\'>Approved Grade: n/a<tpl else>Approved Grade: n/a</tpl></div>',
-            '</div>',
-        '</div>'
+        itemTpl:[
+            '<table width="100%"><tr> <td width="60%" align="left">{Name} {LastName}</td> <td width="20%" align="right">{RawGrade}</td> <td width="20%" align="right">{ApprovedGrade}</td> </tr></table>'
         ],
         store: 'Livestats'
         //onItemDisclosure: false,
