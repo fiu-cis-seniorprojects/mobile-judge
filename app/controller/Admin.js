9869a1fbbf026e80f5d793c2c9b50980843c346e
ejo001 <ejo001@fiu.edu>
2014-07-17 15:01:29 -0400

    Fixed ordering

--- app/controller/Admin.js
+++ app/controller/Admin.js
@@ -884,7 +884,7 @@ Ext.define('OnlineJudges.controller.Admin', {
             GradeSaveBtn = this.getGradeSaveBtn();
             GradeSaveBtn.hide();
         }
-        else if (title === 'Pending Grades'){
+        else if (title === 'Grades'){
             navBtn.from = "pendingGradesTab";
             navBtn.setText('');
             navBtn.setIconCls('');
